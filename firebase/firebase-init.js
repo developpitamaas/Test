5fb31c7d3f86c67213834627e0b61c237de5f10d
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-12-25 10:10:36 +0530

    update fire base

--- firebase/firebase-init.js
+++ firebase/firebase-init.js
@@ -2,10 +2,10 @@ const FirebaseAdmin = require("firebase-admin");
 
 module.exports = FirebaseAdmin.initializeApp({
     credential: FirebaseAdmin.credential.cert({
-      projectId: process.env.project_id,
-      clientEmail: process.env.client_email,
-      privateKey: process.env.private_key
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
     }),
-    projectId: process.env.project_id
+    projectId: process.env.FIREBASE_PROJECT_ID
   });
   
